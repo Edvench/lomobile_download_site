cc288dd3c630f3e580aaa3017c701e85a6aa7c56
Edvench <eduardtovstuha@gmail.com>
2021-10-20 17:38:25 +0300

    upload image

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MockService } from 'src/app/services/mock.service';
 
 @Component({
@@ -7,64 +7,64 @@ import { MockService } from 'src/app/services/mock.service';
   styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
-  public gallery:any  = [
+  public gallery: any = [
     {
-      path:'././assets/img/photo_1.jpg'
+      path: '././assets/img/photo_1.jpg'
     },
     {
-      path:'././assets/img/photo_2.jpg'
+      path: '././assets/img/photo_2.jpg'
     },
     {
-      path:'././assets/img/photo_3.jpg'
+      path: '././assets/img/photo_3.jpg'
     },
     {
-      path:'././assets/img/photo_4.jpg'
+      path: '././assets/img/photo_4.jpg'
     },
     {
-      path:'././assets/img/photo_5.jpg'
+      path: '././assets/img/photo_5.jpg'
     },
     {
-      path:'././assets/img/photo_6.jpg'
+      path: '././assets/img/photo_6.jpg'
     },
     {
-      path:'././assets/img/photo_1.jpg'
+      path: '././assets/img/photo_1.jpg'
     },
     {
-      path:'././assets/img/photo_2.jpg'
+      path: '././assets/img/photo_2.jpg'
     },
     {
-      path:'././assets/img/photo_3.jpg'
+      path: '././assets/img/photo_3.jpg'
     },
     {
-      path:'././assets/img/photo_4.jpg'
+      path: '././assets/img/photo_4.jpg'
     },
     {
-      path:'././assets/img/photo_5.jpg'
+      path: '././assets/img/photo_5.jpg'
     },
     {
-      path:'././assets/img/photo_6.jpg'
+      path: '././assets/img/photo_6.jpg'
     },
     {
-      path:'././assets/img/photo_1.jpg'
+      path: '././assets/img/photo_1.jpg'
     },
     {
-      path:'././assets/img/photo_2.jpg'
+      path: '././assets/img/photo_2.jpg'
     },
     {
-      path:'././assets/img/photo_3.jpg'
+      path: '././assets/img/photo_3.jpg'
     },
     {
-      path:'././assets/img/photo_4.jpg'
+      path: '././assets/img/photo_4.jpg'
     },
     {
-      path:'././assets/img/photo_5.jpg'
+      path: '././assets/img/photo_5.jpg'
     },
     {
-      path:'././assets/img/photo_6.jpg'
+      path: '././assets/img/photo_6.jpg'
     },
   ];
 
-  constructor(private mocks:MockService) { }
+  constructor(private mocks: MockService) { }
 
   ngOnInit(): void {
     console.log(this.gallery)
@@ -72,6 +72,22 @@ export class SliderComponent implements OnInit {
 
   public deleteEl(slide: any, i: any): any {
     this.gallery.splice(i, 1);
+  }
+
+  public onFileChanged(event: any) {
+    const endpoint = '././assets/img/';
+    let file: any;
+    let fileName:any;
+    const fileList: FileList = event.target.files || [];
+    let number = fileList.length
+    console.log(number)
+    for (let i = 0; i < number; i++) {
+      file = event.target.files[i]
+      fileName = file.name
+      console.log(fileName)
+      this.gallery.push(file)
+    }
+    console.log(this.gallery)
   }
 
 }
